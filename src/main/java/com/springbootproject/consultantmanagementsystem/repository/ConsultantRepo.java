5af843a84dc07076307fdb103052059a74e66a8a
Shayana Shrestha <shayana0324@gmail.com>
2026-08-12 09:41:46 -0400

    Cosnultant repository

---
+++ src/main/java/com/springbootproject/consultantmanagementsystem/repository/ConsultantRepo.java
@@ -0,0 +1,17 @@
+/*
+* This is a data access layer
+* Extending JpaRepository
+* We can write any SQL or JDBC code for basic CRUB
+* */
+package com.springbootproject.consultantmanagementsystem.repository;
+
+import com.springbootproject.consultantmanagementsystem.entity.Consultant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultantRepo extends JpaRepository<Consultant, Long> {
+    Page<Consultant> find(String name, String technology, Pageable pageable);
+
+    long countByStatus(String status);
+}
